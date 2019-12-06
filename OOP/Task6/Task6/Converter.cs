71188d8f5b74ea7520667d41de98f889c59f0a6a
IhorShulha <ihors914@gmail.com>
2019-12-06 13:10:44 +0200

    Task6

---
+++ OOP/Task6/Task6/Converter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task6
+{
+    class Converter
+    {
+        public double USD { get; set; }
+        public double EUR { get; set; }
+        public double RUB { get; set; }
+        public double UAH { get; set; }
+
+        public Converter(double usd, double eur, double rub)
+        {
+            USD = usd * 23.96;
+            EUR = eur * 26.05;
+            RUB = rub * 0.37;
+        }
+
+        public Converter(double uah)
+        {
+            USD = uah / 23.96;
+            EUR = uah / 26.05;
+            RUB = uah / 0.37;
+        }
+    }
+}
---
+++ OOP/Task6/Task6/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Task6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Создать класс Converter.
+             * В теле класса создать пользовательский конструктор, который принимает три вещественных аргумента, и
+             * инициализирует поля соответствующие курсу 3-х основных валют, по отношению к гривне - 
+             * public Converter(double usd, double eur, double rub).
+             * Написать программу, которая будет выполнять конвертацию из гривны в одну из указанных валют, также
+             * программа должна производить конвертацию из указанных валют в гривну.
+             */
+
+            Console.WriteLine("Введите сумму в долларах:");
+            double usd = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите сумму в евро:");
+            double eur = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите сумму в рублях:");
+            double rub = Double.Parse(Console.ReadLine());
+
+            Converter converter = new Converter(usd, eur, rub);
+            Console.WriteLine($"{usd} USD = {converter.USD} UAH; \n{eur} EUR = {converter.EUR} UAH;  \n{rub} RUB = {converter.RUB} UAH.");
+            //Console.ReadKey();
+            //Console.Clear();
+
+            Console.WriteLine("Введите сумму в гривнях:");
+            double uah = Double.Parse(Console.ReadLine());
+            Converter convertUAH = new Converter(uah);
+            Console.WriteLine($"{uah} UAH = {convertUAH.USD} USD; \n{uah} UAH ={convertUAH.EUR} EUR; \n{uah} UAH = {convertUAH.RUB} RUB.");
+        }
+    }
+}
