389a913b8172b8d12c571827a9c88383bddf0aa7
IhorShulha <ihors914@gmail.com>
2019-12-06 14:11:50 +0200

    Task7

---
+++ OOP/Task7/Task7/Employee.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task7
+{
+    class Employee
+    {
+        double tax = 0.2;
+        double salary = 1000;
+
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public double Experience { get; set; };
+        public string Position { get; set; }
+
+
+        public Employee(string name, string surname)
+        {
+            Name = name;
+            Surname = surname;
+        }
+
+        public void calculateSalary()
+        {
+            if (Position == "Worker" && Experience <= 1)
+            {
+                salary *= Experience;
+                Console.WriteLine($"Worker с опытом работы {Experience} получает {salary} до вычета налогов или чистыми {salary-(salary * tax)}");
+            }
+            else if (Position == "Worker" && Experience > 1)
+            {
+                salary *= 1.5;
+                Console.WriteLine($"Worker с опытом работы {Experience} получает {salary} до вычета налогов или чистыми {salary - (salary * tax)}");
+            }
+            else if (Position == "Manager" && Experience < 1)
+            {
+                salary *= 2;
+                Console.WriteLine($"Manager с опытом работы {Experience} получает {salary} до вычета налогов или чистыми {salary - (salary * tax)}");
+            }
+            else if (Position == "Manager" && Experience > 2)
+            {
+                salary *= 3;
+                Console.WriteLine($"Manager с опытом работы {Experience} получает {salary} до вычета налогов или чистыми {salary - (salary * tax)}");
+            }
+        }
+    }
+}
---
+++ OOP/Task7/Task7/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Task7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Создать класс Employee.
+             * В теле класса создать пользовательский конструктор, который принимает два строковых аргумента, и
+             * инициализирует поля, соответствующие фамилии и имени сотрудника.
+             * Создать метод рассчитывающий оклад сотрудника (в зависимости от должности и стажа) и налоговый сбор.
+             * Написать программу, которая выводит на экран информацию о сотруднике (фамилия, имя, должность), оклад и налоговый сбор.
+             */
+
+            Employee worker1 = new Employee("Vasya", "Pupkin");
+            Console.Write($"{worker1.Name} {worker1.Surname } - ");
+            worker1.Position = "Worker";
+            worker1.Experience = 1;
+            worker1.calculateSalary();
+            Console.WriteLine();
+
+            Employee worker2 = new Employee("Vova", "Pupkin");
+            Console.Write($"{worker2.Name} {worker2.Surname } - ");
+            worker2.Position = "Worker";
+            worker2.Experience = 5;
+            worker2.calculateSalary();
+            Console.WriteLine();
+
+            Employee manager1 = new Employee("Ivan", "Ivanov");
+            Console.Write($"{manager1.Name} {manager1.Surname } - ");
+            manager1.Position = "Manager";
+            manager1.Experience = 3;
+            manager1.calculateSalary();
+            Console.WriteLine();
+
+            //Console.ReadKey();
+        }
+    }
+}
