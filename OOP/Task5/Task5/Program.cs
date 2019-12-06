69205c398353450121b8d545d30a3296e822de15
IhorShulha <ihors914@gmail.com>
2019-12-06 12:24:56 +0200

    task5

---
+++ OOP/Task5/Task5/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Task5
+{
+    class Program
+    {
+        static string _myString(string variableName)
+        {
+            Console.Write("Enter your " + variableName + ": ");
+            return Console.ReadLine();
+
+        }
+
+        static void Main(string[] args)
+        {
+            /*
+             * Создать класс User, содержащий информацию о пользователе (логин, имя, фамилия, возраст, дата заполнения анкеты). 
+             * Поле дата заполнения анкеты должно быть проинициализировано только один раз (при создании
+             * экземпляра данного класса) без возможности его дальнейшего изменения.
+             * Реализуйте вывод на экран информации о пользователе.
+             */
+
+            string login = _myString("login");
+            string name = _myString("name");
+            string surName = _myString("surName");
+            byte age = Convert.ToByte(_myString("age"));
+            Console.Clear();
+
+            User u = new User(login, name, surName, age);
+            Console.WriteLine(u.ToString());
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ OOP/Task5/Task5/User.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task5
+{
+    class User
+    {
+        string login;
+        string name;
+        string surName;
+        byte age;
+        DateTime time;
+
+        public byte Age
+        {
+            get
+            {
+                return age;
+            }
+
+            set
+            {
+                if (value>0)
+                {
+                    age = value;
+                }
+            }
+        }
+
+        public User(string login, string name, string surName, byte age)
+        {
+            this.login = login;
+            this.name = name;
+            this.surName = surName;
+            this.age = age;
+            time = DateTime.Now;
+        }
+
+        public override string ToString()
+        {
+            return "Login: " + login + "\nName: " + name + "\nSurname: " + surName + "\nAge: " + age + "\nTime Registration: " + time;
+        }
+    }
+}
