c6223573485104f90fff29d983e5561f17b5bcd8
IhorShulha <ihors914@gmail.com>
2019-12-21 21:20:24 +0200

    HomeWork

---
+++ inheritance/Task4/Task4/ProDocumentWorker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task4
+{
+    class ProDocumentWorker : DocumentWorker
+    {
+        public void OpenDocument()
+        {
+            Console.WriteLine("Документ отредактирован");
+        }
+
+        public void EditDocument()
+        {
+            Console.WriteLine("Документ сохранен в старом формате, cохранение в остальных форматах доступно в версии Эксперт");
+        }
+    }
+}
---
+++ inheritance/Task4/Task4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            DocumentWorker documentWorker = new DocumentWorker();
+            ProDocumentWorker proDocumentWorker = new ProDocumentWorker();
+            ExpertDocumentWorker expertDocumentWorker = new ExpertDocumentWorker();
+            
+            string key = Console.ReadLine();
+            
+            switch (key)
+            {
+                case "1":
+                    proDocumentWorker = new ProDocumentWorker();
+                    break;
+                case "2":
+                    expertDocumentWorker = new ExpertDocumentWorker();
+                    break;
+                default:
+                    documentWorker = new DocumentWorker(); ;
+                    break;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
